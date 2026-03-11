dd058e72405ba101d7ae6a7b97e98aa985197dd9
Andy Carta - ADMIN <andycarta@Andys-MacBook-Pro.local>
2026-03-11 11:03:26 -0600

    Revert to 9am/4pm schedule

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from zoneinfo import ZoneInfo
 from bot import run_training, run_production, build_queue, post_from_queue, run_review_mode
 
 CHICAGO_TZ = ZoneInfo("America/Chicago")
-POSTING_HOURS = [9, 12, 16]  # 12 added temporarily for testing
+POSTING_HOURS = [9, 16]  # 9 AM and 4 PM Chicago time
 
 def get_chicago_time():
     return datetime.now(CHICAGO_TZ)
